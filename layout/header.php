6ba6f7364a1a439202cb5b93a9b32917031ce2b8
Hamdy Salah <hamdysalah.dev@gmail.com>
2025-04-25 09:45:28 +0300

    updated

--- layout/header.php
+++ layout/header.php
@@ -7,7 +7,7 @@ $current_page = basename($_SERVER['PHP_SELF']);
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>BrewBuzz - Top Coffee Picks</title>
-  <link rel="stylesheet" href="styles.css">
+<link rel="stylesheet" href="../src/styles1.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 <body>
